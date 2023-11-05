91a131506a84f491e9c36e85f1a90f590d6856fa
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-05 17:45:26 +0600

    token logger and verify middleware and get jobs from database created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,8 +8,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9wwku4y.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,12 +28,72 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middleware own created
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Internal Server Error');
+});
+
+const logger = async (req, res, next) => {
+  console.log('called', req.host, req.originalUrl);
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log('value of token in middleware', token);
+  if (!token) {
+    return res.status(401).send({ message: 'not authorized' });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //error
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: 'unauthorized' });
+    }
+    // if token is valid then it would be decoded
+    console.log('value in the token', decoded);
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
     const jobsCollection = client.db('skillSwapr').collection('allJobs');
+
+    // auth related api
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      console.log('user for token', user);
+      // node
+      // require('crypto').randomBytes(64).toString('hex')
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          sameSite: 'none',
+          secure: true,
+        })
+        .send({ success: true });
+    });
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log('logging out', user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true });
+    });
+
+    // jobs related api
+    app.get('/jobs', logger, async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
